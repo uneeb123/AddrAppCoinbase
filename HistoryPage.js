90fb25126b8b736e40b25d16ef78e40279c80d5c
uneeb123 <uneeb.i.am@gmail.com>
2018-04-05 22:34:19 -0700

    Random

--- HistoryPage.js
+++ HistoryPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
   Text,
+  View,
+  StyleSheet
 } from 'react-native';
 
 export default class HistoryPage extends Component<{}> {
@@ -13,7 +15,26 @@ export default class HistoryPage extends Component<{}> {
     const user = params ? params.user : null;
 
     return (
-      <Text>Hello, {user.name}!</Text>
+      <View>
+        <View style={styles.account_container}>
+          <Text>Hello, {user.name}!</Text>
+          <Text>Current balance: {user.balance} {user.currency_code}</Text>
+          <Text>Address: {user.address}</Text>
+        </View>
+        <View style={styles.history_container}>
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  account_container: {
+    margin: 10,
+    flex: 1,
+  },
+  history_container: {
+    margin: 10,
+    flex: 2,
+  }
+});
