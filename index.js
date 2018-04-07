f5b155e3b06f3b0dca90eebd9d05623391e2115e
uneeb123 <uneeb.i.am@gmail.com>
2018-04-06 21:05:45 -0700

    Renamed project and added icon

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('PropertyFinder', () => App);
+AppRegistry.registerComponent('Addr', () => App);
